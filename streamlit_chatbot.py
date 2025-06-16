692ce4128bd77cdce6923f848a5aa00350d722f2
swathiselvaraj <swati.selvaraj11@gmail.com>
2025-06-16 13:53:22 +0200

    c

--- streamlit_chatbot.py
+++ streamlit_chatbot.py
@@ -458,8 +458,8 @@ def validate_followup(user_input: str, question_id: str, options: List[str], que
         #     return "Hello! I can help with survey questions. What would you like to know?"
 
 
-        if len(user_input) == 1:
-            return "Please ask a question related to the survey."
+        # if len(user_input) == 1:
+        #     return "Please ask a question related to the survey."
 
 
 
