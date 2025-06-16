02750d4ed903f5eaf8d3452aaf9ded2299864f08
swathiselvaraj <swati.selvaraj11@gmail.com>
2025-06-16 14:23:49 +0200

    c

--- streamlit_chatbot.py
+++ streamlit_chatbot.py
@@ -451,7 +451,7 @@ def validate_followup(user_input: str, question_id: str, options: List[str], que
         #     st.session_state.last_recommendation = None
 
         #     
-            
+        placeholder = st.empty()     
 
         greetings = {"hi", "hello", "hey","how are you", "I have a question", "greetings"}
         user_clean = user_input.lower().strip().strip(string.punctuation)
